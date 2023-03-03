cdbc31cae91af85e242262f3eaa144d6ee3d82ea
BeatrizHCavalcanti <124796589+BeatrizHCavalcanti@users.noreply.github.com>
2023-03-03 12:54:48 -0300

    Delete cypress directory

--- cypress/page/login/login.elements.ts
+++
@@ -1,8 +0,0 @@
-export const loginElements = {
-    // Login Page
-    txt_user: 'input[id="mat-input-0"]',
-    txt_password: 'input[id="mat-input-1"]',
-    btn_log_in: 'button[type="submit"]',
-}
-
-export default loginElements;
